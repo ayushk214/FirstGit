2a5dbc7d2f91d86bf34827eaa592e61d1e6653b2
ayushk214 <ayushkumargaurav214@gmail.com>
2023-08-24 22:20:46 +0530

    2nd list text color green and made 3rd elementHide

--- DOM.js
+++ DOM.js
@@ -2,13 +2,13 @@
 console.dir(document);
 console.log(document.domain);
 console.log(document.title);
-//document.title=123;
+//document.title=123;//
 console.log(document.head);
 console.log(document.body);
 console.log(document.all);
 console.log(document.doctype);
 
-//SELECTORS
+//SELECTORS//
 
 document.all[10].textContent="Hello";
 
@@ -34,13 +34,56 @@ console.log(li[1]);
 li[2].style.fontWeight="bold";
 
 console.log(li[2]);
-// items[2].style.backgroundColor="green"
+// items[2].style.backgroundColor="green"//
 
 for(var i=0; i<li.length; i++){
     li[i].style.fontWeight="bold";
 }
 
-li[1].style.backgroundColor="green"
+//li[1].style.backgroundColor="green"//
 
+//QUERYSELECTOR//
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+
+//adding value in the addItem box//
+var input=document.querySelector('input');
+input.value='Hello World'
+
+var submit=document.querySelector('input[type="submit"]');
+submit.value="SEND"
+
+//changing the colour of firstList Item//
+
+var firstItem=document.querySelector('.list-group-item');
+firstItem.style.color='red';
+
+//changing the color of the LastItem//
+
+var LastItem=document.querySelector('.list-group-item:last-child');
+LastItem.style.color="blue";
+
+//changing color of second Item//
+
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color="green";
+
+//QUERYSELECTORALL//
+
+var titles=document.querySelectorAll('.title');
+console.log(titles)
+titles[0].textContent='Hello'
+
+//background color changing of even/odd list//
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i<odd.length;i++){
+    odd[i].style.backgroundColor="#f4f4f4";
+}
+
+// Select the 3rd li element (index 2) within the #myList element///
+const thirdListItem = document.querySelectorAll('li')[2];
+
+// Hide the 3rd li element
+thirdListItem.style.display = 'none';
 
 
