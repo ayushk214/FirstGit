f1072efbf16d969a7cfc938cff349d26738477cf
ayushk214 <ayushkumargaurav214@gmail.com>
2023-08-24 21:43:49 +0530

    get element by tag name(li[2]==green)

--- DOM.js
+++ DOM.js
@@ -13,21 +13,34 @@ console.log(document.doctype);
 document.all[10].textContent="Hello";
 
 //STYLING
-var headerTitle =document.getElementById('header-title');
-headerTitle.textContent="goodbye"
-headerTitle.style.borderBottom="solid 3px #000"
+// var headerTitle =document.getElementById('header-title');
+// headerTitle.textContent="goodbye"
+// headerTitle.style.borderBottom="solid 3px #000"
 
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[2].style.fontWeight="bold";
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[2].style.fontWeight="bold";
 
-console.log(items[2]);
-items[2].style.backgroundColor="green"
+// console.log(items[2]);
+// items[2].style.backgroundColor="green"
 
-for(var i=0; i<items.length; i++){
-    items[i].style.fontWeight="bold";
+// for(var i=0; i<items.length; i++){
+//     items[i].style.fontWeight="bold";
+// }
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[2].style.fontWeight="bold";
+
+console.log(li[2]);
+// items[2].style.backgroundColor="green"
+
+for(var i=0; i<li.length; i++){
+    li[i].style.fontWeight="bold";
 }
+
+li[1].style.backgroundColor="green"
 
 
 
