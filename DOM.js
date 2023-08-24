a51e369a548dff6f8da86cb04de2470e9b5713e3
ayushk214 <ayushkumargaurav214@gmail.com>
2023-08-24 19:54:41 +0530

    all list item is bold

--- DOM.js
+++ DOM.js
@@ -20,11 +20,14 @@ headerTitle.style.borderBottom="solid 3px #000"
 var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
-items[1].style.fontWeight="bold";
+items[2].style.fontWeight="bold";
 
 console.log(items[2]);
 items[2].style.backgroundColor="green"
 
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight="bold";
+}
 
 
 
