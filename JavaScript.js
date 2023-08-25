e737cf8ee9de5c0fd9ce75c7ec6a8a3033c3396f
ayushk214 <ayushkumargaurav214@gmail.com>
2023-08-25 23:10:44 +0530

    adding delete and edit button

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,63 @@
-var form= document.g
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+// Add item
+function addItem(e) {
+    e.preventDefault();
+
+    // Get input value
+    var newItem = document.getElementById('item').value;
+
+    // Create new li element
+    var li = document.createElement('li');
+
+    // Add class
+    li.className = 'list-group-item';
+
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // Create delete button element
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Create edit button element
+    var editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-primary btn-sm float-right edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+    // Append buttons to li
+    li.appendChild(deleteBtn);
+    li.appendChild(editBtn);
+
+    // Append li to list
+    itemList.appendChild(li);
+}
+
+// Delete item
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure you want to delete this item?')) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+
